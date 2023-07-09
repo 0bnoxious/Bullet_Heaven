c8b1f7806228aa8f74a1b18b86fa1263a4ea0e69
0bnoxious <n.beaudet234@gmail.com>
2023-07-09 15:03:32 -0400

    added population movement update

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 use rand::Rng;
 
 pub const PERSONCOUNT: i32 = 50;
-pub const PERSONSPEED: f32 = 500.;
+pub const PERSONSPEED: f32 = 50.;
 
 fn main() {
     App::new()
@@ -13,16 +13,11 @@ fn main() {
         .add_startup_system(setup)
         .add_startup_system(populate)
         .add_system(move_population)
+        .add_system(update_population_direction)
         .run()
 }
 
-pub fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    commands.spawn(Person { is_infected: false });
-
+pub fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 
     commands.insert_resource(TimerRes {
@@ -38,13 +33,10 @@ struct TimerRes {
 #[derive(Component)]
 pub struct Person {
     pub is_infected: bool,
+    pub direction: Vec3,
 }
 
-pub fn populate(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
+pub fn populate(mut commands: Commands) {
     let mut n = 0;
 
     while n < PERSONCOUNT {
@@ -63,26 +55,34 @@ pub fn populate(
                 transform: Transform::from_translation(Vec3::new(numx as f32, numy as f32, 0.)),
                 ..default()
             },
-            Person { is_infected: false },
+            Person {
+                is_infected: false,
+                direction: Vec3 {
+                    x: 0.,
+                    y: 0.,
+                    z: 0.,
+                },
+            },
         ));
 
         n += 1;
     }
 }
 
-fn move_population(
-    mut query: Query<
-        // components returned
-        &mut Transform,
-        // filter
-        With<Person>,
-    >,
+fn move_population(mut query: Query<(&mut Transform, &Person)>, time: Res<Time>) {
+    for (mut transform, person) in &mut query.iter_mut() {
+        transform.translation += person.direction * PERSONSPEED * time.delta_seconds();
+    }
+}
+
+fn update_population_direction(
+    mut query: Query<(&mut Person)>,
     time: Res<Time>,
     mut timer_res: ResMut<TimerRes>, //metton jveu faire pause?
 ) {
     timer_res.timer.tick(time.delta());
 
-    for mut transform in &mut query {
+    for (mut person) in &mut query {
         if timer_res.timer.just_finished() {
             let mut direction = Vec3::new(0., 0., 0.);
             let numx = rand::thread_rng().gen_range(-1..=1);
@@ -90,13 +90,14 @@ fn move_population(
             println!("numx : {}   numy : {}", numx.to_string(), numy.to_string());
             direction += Vec3::new(numx as f32, numy as f32, 0.);
 
-            transform.translation += direction * PERSONSPEED * time.delta_seconds();
+            person.direction = direction * PERSONSPEED * time.delta_seconds();
         }
     }
 }
 
 pub fn infect(query: Query<&Person>) {
     for person in &query {
+        if (person.is_infected == true) {}
         println!("Is infected? : {}", person.is_infected);
     }
 }
